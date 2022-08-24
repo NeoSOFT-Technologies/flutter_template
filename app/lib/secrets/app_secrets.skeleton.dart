afd2da588a3fa489334a0817c7b223875831c159
faiyyazs <faiyyazshaikh2010@gmail.com>
2022-08-24 10:51:45 +0530

    Code Clean up & directory rules

---
+++ app/lib/secrets/app_secrets.skeleton.dart
@@ -0,0 +1,20 @@
+/*
+ class AppSecrets {
+  final String thirdpartyApiKey;
+
+  const AppSecrets({
+    required this.thirdpartyApiKey,
+  });
+
+  static const appSecretsDev = AppSecrets(
+   thirdpartyApiKey: "Your API Key",
+  );
+
+  static const appSecretsQA = AppSecrets(
+   thirdpartyApiKey: "Your API Key",
+  );
+
+  static const appSecretsProd = AppSecrets(
+   thirdpartyApiKey: "Your API Key",
+  );
+ }*/
