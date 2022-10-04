b7d6b03caaf610e70ea8841a3d6976e8800a5f3b
faiyyazs <faiyyazshaikh2010@gmail.com>
2022-10-04 19:14:16 +0530

    Updating Error Module configs to support fallback values

--- app/lib/errors/flutter_alert_error_presenter.dart
+++ app/lib/errors/flutter_alert_error_presenter.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_errors/flutter_errors.dart';
 
-class FlutterAlertErrorPresenter extends AlertErrorPresenter<String> {
+import '../model/alert_texts.dart';
+
+class FlutterAlertErrorPresenter<T extends AlertTexts>
+    extends AlertErrorPresenter<T> {
   FlutterAlertErrorPresenter(
       {super.alertTitle = "", super.positiveButtonText = ""});
 
   @override
-  void show(Exception throwable, BuildContext context, String data) {
+  void show(Exception throwable, BuildContext context, AlertTexts data) {
     // your alert provider
   }
 }
