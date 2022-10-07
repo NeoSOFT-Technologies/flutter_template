10ff3fcf3202fb646518923dd12812a3418bf872
Mayuri Patil <patilmayuri573@gmail.com>
2022-10-07 11:51:09 +0530

    --added fonts in theme package and updated theme data

--- app/lib/myapp.dart
+++ app/lib/myapp.dart
@@ -15,6 +15,20 @@ class MyApp extends StatelessWidget {
     return ThemeBuilder(
       themes: [
         ThemeData(
+            textTheme: const TextTheme(
+                bodyLarge: TextStyle(fontFamily: 'Poppins'),
+                bodyMedium: TextStyle(fontFamily: 'Poppins'),
+                bodySmall: TextStyle(fontFamily: 'Poppins'),
+                displayLarge: TextStyle(fontFamily: 'Poppins'),
+                displayMedium: TextStyle(fontFamily: 'Poppins'),
+                displaySmall: TextStyle(fontFamily: 'Poppins'),
+                titleLarge: TextStyle(fontFamily: 'Poppins'),
+                titleMedium: TextStyle(fontFamily: 'Poppins'),
+                titleSmall: TextStyle(fontFamily: 'Poppins'),
+                labelLarge: TextStyle(fontFamily: 'Poppins'),
+                labelMedium: TextStyle(fontFamily: 'Poppins'),
+                labelSmall: TextStyle(fontFamily: 'Poppins'),
+            ),
             backgroundColor: Colors.red,
             colorScheme: ColorScheme.fromSwatch().copyWith(
               secondary: Colors.blue,
